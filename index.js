0042987b41335f4bac858c49ac6a6f06978dbf17
mehsan <mehsanfahad@gmail.com>
2023-05-25 18:34:09 +0530

    learning java script

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-document.getElementById("count").innerText = 5
+// document.getElementById("count").innerText = 5
+
+let mee=26
+let mon=07
+let year=2002
+console.log(mee)
+console.log(mon)
+console.log(year)
