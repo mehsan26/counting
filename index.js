53be7d1e1cca6d011e0377cf55d6ea353d8d2d2e
mehsan <mehsanfahad@gmail.com>
2023-05-25 20:54:26 +0530

    added increment function

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 // document.getElementById("count").innerText = 5
 
-let mee=26
-let mon=07
-let year=2002
-console.log(mee)
-console.log(mon)
-console.log(year)
+// let mee=26
+// let mon=07
+// let year=2002
+// console.log(mee)
+// console.log(mon)
+// console.log(year)
+
+// let lapsCompleted = 0
+
+// Create a function that increments the lapsCompleted variable with one
+// Run it three times
+
+// function incrementLap() {
+//     lapsCompleted = lapsCompleted + 1
+//     console.log(lapsCompleted)
+// }
+
+// incrementLap()
+// incrementLap()
+// incrementLap()
+
+// console.log(lapsCompleted)
+
+
+let countEl = document.getElementById("count-el")
+console.log(countEl)
+let count=0
+function increment() {
+    count=count+1
+    countEl.innerText=count
+}
