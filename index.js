58cd4890c55863205b353042bcbebf934b0bdc9e
mehsan <mehsanfahad@gmail.com>
2023-05-26 23:48:20 +0530

    added save button

--- index.js
+++ index.js
@@ -25,9 +25,16 @@
 
 
 let countEl = document.getElementById("count-el")
-console.log(countEl)
+// console.log(countEl)
 let count=0
 function increment() {
     count=count+1
     countEl.innerText=count
 }
+
+let saveEl=document.getElementById("save-el")
+let entry=" " + count + " -"
+function save() {
+    saveEl.innerText=entry
+}
+
